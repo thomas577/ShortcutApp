7f388a407a35ae742e10c33080d76fab25be1c8a
Thomas Dier <Thomas.Dier@tgptrading.glb.corp.local>
2018-05-30 13:44:28 +0100

    Creating the shell

---
+++ ShortcutCarousel.Shell/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ShortcutCarousel.Shell
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+        public App()
+        {
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(string.Format("An error occured: {0}", e.Exception.Message), "Error");
+            e.Handled = true;
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			// The boostrapper will create the Shell instance, so the App.xaml does not have a StartupUri.
+			Bootstrapper bootstrapper = new Bootstrapper();
+			bootstrapper.Run();
+		}
+	}
+}
---
+++ ShortcutCarousel.Shell/Bootstrapper.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Practices.Prism.MefExtensions;
+using Microsoft.Practices.Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.ServiceLocation;
+
+namespace ShortcutCarousel.Shell
+{
+	public class Bootstrapper : MefBootstrapper
+	{
+		protected override DependencyObject CreateShell()
+		{
+			return ServiceLocator.Current.GetInstance<Shell>();
+		}
+
+		protected override void InitializeShell()
+		{
+			base.InitializeShell();
+            
+			Application.Current.MainWindow = (Shell)this.Shell;
+			Application.Current.MainWindow.Show();
+		}
+
+		protected override void ConfigureAggregateCatalog()
+		{
+			base.ConfigureAggregateCatalog();
+
+			// Add this assembly to export ModuleTracker
+			this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(Bootstrapper).Assembly));
+
+			// Module A is referenced in in the project and directly in code.
+			//this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(ModuleA).Assembly));
+			//this.AggregateCatalog.Catalogs.Add(new AssemblyCatalog(typeof(ModuleC).Assembly));
+
+			// Module B and Module D are copied to a directory as part of a post-build step.
+			// These modules are not referenced in the project and are discovered by inspecting a directory.
+			// Both projects have a post-build step to copy themselves into that directory.
+			//DirectoryCatalog catalog = new DirectoryCatalog("DirectoryModules");
+			//this.AggregateCatalog.Catalogs.Add(catalog);
+		}
+
+		protected override IModuleCatalog CreateModuleCatalog()
+		{
+			// When using MEF, the existing Prism ModuleCatalog is still the place to configure modules via configuration files.
+			return new ConfigurationModuleCatalog();
+		}
+	}
+}
